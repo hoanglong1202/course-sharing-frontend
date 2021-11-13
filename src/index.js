f16b010d0165d1ee875ad457d9f824c82a1550eb
Accel <cyberpunk2076@gmail.com>
2021-11-13 22:21:25 +0700

    init frontend

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <BrowserRouter>
+        <SnackbarProvider
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          maxSnack={3}
+        >
+          <App />
+        </SnackbarProvider>
+      </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
