49ee481de8adbf1b628035f6ad07ae4df8226015
Accel <cyberpunk2076@gmail.com>
2022-01-04 19:53:25 +0700

    add facebook validate URL

--- src/utils/common.js
+++ src/utils/common.js
@@ -18,3 +18,18 @@ export const validateYouTubeUrl = (url) => {
     }
   }
 };
+
+export const validateFacebookUrl = (url) => {
+  if (url !== undefined || url !== "") {
+    // eslint-disable-next-line no-useless-escape
+    let regExp = /^(?:(?:https?:)?\/\/)?(?:www\.)?facebook\.com\/[a-zA-Z0-9\.]+\/videos\/(?:[a-zA-Z0-9\.]+\/)?([0-9]+)/;
+    let match = url.match(regExp);
+    if (match && match[1].length > 0) {
+      let embedUrl = `https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2Fvideos%2F${match[1]}%2F&width=500&show_text=false&height=300&appId`;
+
+      return embedUrl;
+    } else {
+      return "";
+    }
+  }
+};
